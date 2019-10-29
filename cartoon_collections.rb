ea070c8284ac6a3c16046714c60ff0c98161130a
Sean Eriksen <eriksensean@yahoo.com>
2019-10-29 18:38:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarves.collect {|x|}
+  puts |x|
 end
 
 def summon_captain_planet# code an argument here
