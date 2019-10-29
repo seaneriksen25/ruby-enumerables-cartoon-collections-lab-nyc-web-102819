6b3e62327928f63c19acb1338c5e41da45488525
Sean Eriksen <eriksensean@yahoo.com>
2019-10-29 19:32:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,15 @@
 def roll_call_dwarves# code an argument here
   # Your code here
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarves.collect {|x|}
-  puts |x|
+  dwarves.collect {|n|}
+  puts |n|
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls.collect { |x| x + "!".upcase}
+  summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls# code an argument here
